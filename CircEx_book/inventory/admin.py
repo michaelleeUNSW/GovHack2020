bbc9ab140b1e9659e922e95e1190967d955ad116
michaellee12 <sklee.mike@gmail.com>
2020-08-15 19:06:42 +0800

    Update

--- CircEx_book/inventory/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from .models import Book, Store, Inventory
-
-admin.site.register([Book, Store, Inventory])
--- CircEx_book/inventory/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-app_name = 'inventory'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:book_id>', views.detail, name='detail'),
-]
